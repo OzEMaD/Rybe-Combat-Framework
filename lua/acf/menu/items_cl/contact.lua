4c86f3e1a78ec077e99b6febbdde3480e6a6d86c
OzEMaD <154848577+OzEMaD@users.noreply.github.com>
2026-02-20 16:04:51 -0600

    Remove CreateMenu function and UI elements
    Removed the CreateMenu function and its associated UI elements.

--- lua/acf/menu/items_cl/contact.lua
+++ lua/acf/menu/items_cl/contact.lua
@@ -1,16 +1,1 @@
-local function CreateMenu(Menu)
-	Menu:AddTitle("Ion care what you think")
-	Menu:AddLabel("For this reason, fuck you")
 
-	do -- How to Contribute
-		local Base = Menu:AddCollapsible("How to Contribute", false)
-
-		Base:AddLabel("To make it easier for first time contributors, we've left a guide about how to contribute to the addon.")
-
-		local Link = Base:AddButton("Contributing to ACF")
-
-		function Link:DoClickInternal()
-			gui.OpenURL("https://github.com/OzEMaD/Rybe-Combat-Framework/blob/main/CONTRIBUTING.md")
-		end
-	end
-end
